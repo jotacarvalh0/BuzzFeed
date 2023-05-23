98e27b512a1f3b2d548135cd2e3d38a0f556dab9
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-05-23 13:03:43 -0300

    Propriedades lógicas

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.sass']
 })
 export class QuizzComponent implements OnInit {
+  
+  title:string = ""
+
+  questions:any 
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
 
   constructor() { }
 
