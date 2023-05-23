4ab0ce626612674f11ffa01e19fdf28e51358a69
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-05-23 13:53:54 -0300

    Calculando o resultado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -38,6 +38,7 @@ export class QuizzComponent implements OnInit {
 
   playerChoose(value:string){
     this.answers.push(value)
+    this.nextStep()
   }
 
   async nextStep(){
@@ -46,8 +47,26 @@ export class QuizzComponent implements OnInit {
     if(this.questionMaxIndex > this.questionIndex){
       this.questionSelected = this.questions[this.questionIndex]
     }else{
+      const finalAnswer:string =  await this.checkResult(this.answers)
       this.finished = true
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof
+      typeof quizz_questions.results]
     }
+  }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i , arr)=>{
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length 
+      ){
+        return previous
+      }else{
+        return current
+      }
+    })
+
+    return result
   }
 
 }
